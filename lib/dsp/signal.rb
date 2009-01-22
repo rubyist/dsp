0857a0ed5245cef8d314a60ab62cf79d2e600883
Scott Barron <scott@elitists.net>
2009-01-22 13:06:03 -0500

    add Signal#cmux

--- lib/dsp/signal.rb
+++ lib/dsp/signal.rb
@@ -12,5 +12,14 @@ module DSP
     def stddev
       Math.sqrt(variance)
     end
+    def cmux(r)
+      smaller, larger = self.size < r.size ? [self, r.dup] : [r.dup, self]
+      pad_amount = (larger.size - smaller.size) / 2
+      pad_amount.times do
+        smaller.unshift nil
+        smaller << nil
+      end
+      larger.zip(smaller)
+    end
   end
 end
--- test/test_signal.rb
+++ test/test_signal.rb
@@ -14,4 +14,36 @@ class TestSignal < Test::Unit::TestCase
       assert_in_delta 3.16227766016838, DSP::Signal.new([1, 2, 3, 4, 5]).stddev, 0.001
     end
   end
+
+  context "cmuxing" do
+    setup do
+      @left = DSP::Signal.new([1, 2, 3, 4, 5])
+      @right = DSP::Signal.new([2, 3, 4])
+    end
+
+    should "not modify the signals" do
+      @left.cmux(@right)
+      assert_equal [2, 3, 4], @right
+    end
+    
+    should "mux signals of the same size" do
+      assert_equal [[2, 2], [3, 3], [4, 4]], @right.cmux(@right)
+    end
+
+    should "mux a smaller signal" do
+      assert_equal [[1, nil], [2, 2], [3, 3], [4, 4], [5, nil]], @left.cmux(@right)
+    end
+
+    should "mux a larger signal" do
+      assert_equal [[1, nil], [2, 2], [3, 3], [4, 4], [5, nil]], @right.cmux(@left)
+    end
+
+    should "mux a smaller signal with odd size" do
+      assert_equal [[2, 1], [3, 2], [4, nil]], @right.cmux([1, 2])
+    end
+
+    should "mux a larger signal with odd size" do
+      assert_equal [[1, 2], [2, 3], [3, 4], [4, nil]], @right.cmux([1, 2, 3, 4])
+    end
+  end
 end
